4a5c87e4aa06c4a96f1b55c0fa0257f9520d9f11
Henry Hoyos <hdosurrego@gmail.com>
2020-01-21 20:11:09 -0500

    Navbar changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,6 +5,7 @@ import Electric from "./Electric";
 import Builders from "./Builders";
 import Management from "./Management";
 import WesworthConstruction from "./img/WesworthConstruction.jpg";
+import "./NavBar.css";
 
 export default class NavBar extends React.Component {
 	render() {
