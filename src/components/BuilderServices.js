e7cdb017df4c64e883f37a7d24b545646b23bcc7
Henry Hoyos <hdosurrego@gmail.com>
2020-01-30 16:38:09 -0500

    builder services started

--- src/components/BuilderServices.js
+++ src/components/BuilderServices.js
@@ -6,17 +6,6 @@ export default class BuilderServices extends React.Component {
 			<div>
 				<div className="commercial-services">
 					<h1>BUILDER SERVICES</h1>
-					<p>
-						Wesworth Electric was founded in 1981 by Mr. Wesley Shreve with the
-						basic objective of serving his community with a great quality and a
-						great customer service. Over the years Wesworth Electric has evolved
-						the array of services we offer, from small residential services to
-						high voltage and complex industrial installations. Nowadays,
-						Wesworth Electric is a division of Wesworth Construction Group
-						spanning customers in two states, and still keeping the basic
-						principles we were founded on: Great Quality and Excellent Customer
-						Service.
-					</p>
 				</div>
 			</div>
 		);
