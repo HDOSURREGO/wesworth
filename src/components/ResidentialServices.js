61ea1c43c7d0836eead43c8f435ac69a1ed028aa
Henry Hoyos <hdosurrego@gmail.com>
2020-02-03 20:30:23 -0500

    residential services started

---
+++ src/components/ResidentialServices.js
@@ -0,0 +1,80 @@
+import React from "react";
+import "./Services.css";
+
+export default class IndustrialServices extends React.Component {
+	render() {
+		return (
+			<div className="service-wrapper">
+				<h2>RESIDENTIAL SERVICES</h2>
+				<hr />
+				<div class="commercial-services">
+					<div class="column-left">
+						<h3>Residential Services</h3>
+						<ul>
+							<li>
+								Industrial Network Design and Hardware Specifications to
+								Industry Standards.
+							</li>
+							<li>Factory Acceptance Testing</li>
+							<li>Project + Construction Management</li>
+							<li>Engineering Documents and Drawings</li>
+							<li>Systems Operation and Maintenance Training</li>
+							<li>Electrical System Safety Analysis and Training</li>
+							<h3>Predictive Maintenance</h3>
+							<li>Perform an all-points audit of electrical systems </li>
+							<li>Infrared Thermal Scanning</li>
+							<li>Offline Electrical Testing</li>
+							<li>Online Motor Testing</li>
+							<li>Power/Current Analysis</li>
+							<li>Surge suppressors installation and maintenance</li>
+							<li>Exit and Emergency lighting installation and maintenance</li>
+							<h3>Benefits</h3>
+							<li>Minimize downtime</li>
+							<li>Up to date status of electrical systems</li>
+							<li>Reduce hazards</li>
+							<li>Improve operator safety</li>
+						</ul>
+					</div>
+					<div class="column-right">
+						<h3>Industry Specific Services</h3>
+						<ul>
+							<li>Buck and boost transformers</li>
+							<li>Motor controllers</li>
+							<li>Low Voltage Systems/Networks</li>
+							<li>Grounding Systems</li>
+							<li>High Voltage Installations</li>
+							<li>Camera/Security/Access Control Systems</li>
+							<li>
+								Interior – Exterior Lighting Systems (including LED Lighting)
+							</li>
+							<li>Equipment Relocation</li>
+							<li>Control/Custom Panel Fabrication</li>
+							<li>
+								Switchgear, Panel Boards, Transformers, Bussway and Transfer
+								Switch Installation
+							</li>
+							<li>Solar Power Analysis/Design and Installation</li>
+							<li>Electrical Vehicle (EV) Chargers Installation</li>
+							<li>
+								Generator/Transfer Switch Installation, Repair and Maintenance
+							</li>
+							<li>Access control design and installation</li>
+							<li>Closed Circuit TV Cameras (CCTV) design and installation</li>
+							<li>
+								Lighting studies to maximize light output and minimize power
+								consumption
+							</li>
+							<li>Phase loss</li>
+						</ul>
+						<h3>Emergency Services</h3>
+						<p>
+							Electrical Failures are disruptive to your business and it is
+							always good to have someone to call. We will be there for you
+							24/7. Call Us – (954) 5804070
+						</p>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
